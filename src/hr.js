64ec70680720978a708a3011f4bf90ac228c560c
shao152222 <shao152222@163.com>
2019-04-21 21:39:36 +0800

    custom horizontal ruler

---
+++ src/hr.js
@@ -0,0 +1,31 @@
+/**
+ * @license Copyright (c) 2003-2017, CKSource - Frederico Knabben. All rights reserved.
+ * For licensing, see LICENSE.md.
+ */
+
+/**
+ * @module pdf/pdf
+ */
+
+import Plugin from '@ckeditor/ckeditor5-core/src/plugin';
+
+import HREditing from './hr/hrediting';
+import HRUI from './hr/hrui';
+
+import '../theme/hr.less';
+
+export default class HRPlugin extends Plugin {
+  /**
+   * @inheritDoc
+   */
+  static get requires() {
+    return [HRUI, HREditing];
+  }
+
+  /**
+   * @inheritDoc
+   */
+  static get pluginName() {
+    return 'HR';
+  }
+}
